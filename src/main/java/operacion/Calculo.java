e55c54845dda5dd34cbf2c6bb36932e64691ff60
Mikaelo <miguel.hdz.mrn@gmail.com>
2020-09-08 21:11:11 -0500

    version 1

---
+++ src/main/java/operacion/Calculo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacion;
+
+/**
+ *
+ * @author mikaelo
+ */
+public class Calculo {
+    
+    public int getRandom(int max, int min){
+        int valorDado = (int) Math.floor(Math.random()*max+min);
+        return valorDado;
+    }
+    public static void main(String args[]){
+        System.out.println(new Calculo().getRandom(5000, 1) );
+    }
+    
+}
---
+++ src/main/java/practica1/Practica1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author roberto
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana1 ventana=new Ventana1 ();
+        ventana.setVisible(true);
+        
+    }
+    
+}
