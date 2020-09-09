0dd6dc7f354e42b92f72d0692298ccd60dc165d8
Mikaelo <miguel.hdz.mrn@gmail.com>
2020-09-08 21:13:47 -0500

    practca 2

---
+++ src/main/java/practica2/Practica2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2;
+
+/**
+ *
+ * @author roberto
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana2 ventana=new Ventana2();
+        ventana.setVisible(true);
+    }
+    
+}
